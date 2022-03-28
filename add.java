ea46ba9a2185a937abb3b583bfd4d4df44eb569a
shubhambarhate23 <101618815+shubhambarhate23@users.noreply.github.com>
2022-03-28 11:50:24 +0530

    donee

---
+++ add.java
@@ -0,0 +1,26 @@
+package math;
+
+import java.util.Scanner;
+
+public class add {
+
+	public static void main(String[] args) {
+     int a;
+     int b;
+     int sum;
+     Scanner s= new Scanner(System.in);
+     System.out.println("ente num : ");
+     a =s.nextInt();
+     System.out.println("ente num : ");
+     b = s.nextInt();
+     s.close();
+     sum = a + b ;
+     System.out.println("sum of num : " +sum);
+
+     
+     
+     
+     
+	}
+
+}
